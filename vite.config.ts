ca14e01fc9354023ebc685f165030fd4567d315d
Muhammad Anas <anasarif216@gmail.com>
2025-03-25 03:01:51 +0500

    fix: generating index.d.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import dts from "vite-plugin-dts"; // Add this
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      // Add the plugin configuration
+      insertTypesEntry: true,
+      rollupTypes: true,
+      exclude: ["**/__tests__/**", "**/*.stories.tsx"],
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
